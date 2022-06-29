9474c9416437c34e85e42e598e6222d11fa9af5b
DS83969 <zhannagolubeva@yahoo.com>
2022-06-29 17:54:51 -0500

    info

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        All to know about coding.
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
